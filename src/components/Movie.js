4d45abf221169ef6111f5bdd3df168834796ca2d
Janell Robisch <59804782+XanderWitch@users.noreply.github.com>
2022-02-19 11:32:45 -0500

    Reviews not showing debugged!

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,41 +1,40 @@
 import React from 'react';
 import movieArray from './MovieArray';
-import reviewArray from './ReviewArray';
 import ReviewForm from './ReviewForm';
 
 const Movie = () => {
 	return (
 		<div className='movie-display'>
-			<ReactFragment>
-				{movieArray.map((movie) => (
-				<div key={movie.id} className='movie-item mt-5 p-2'>
-					<img
-						className='card-img-top'
-						src={movie.movieImage}
-						alt='movie poster'
-					/>
-					<p className='movie-listing'>
-						{movie.movieName}
-						<br />
-						{movie.movieGenre}, {movie.releaseDate},
-						<br />
-						Rated {movie.movieRating}
-						<br />
-					</p>
-				</div>
-					{movie.reviews.map((review) => (
-						<div key={review.id}>
-							{review.movieRating}<br />
-							"{review.review}"<br />
-							—{review.username}, {review.currentDate}
+			{movieArray.map((movie) => (
+				<React.Fragment>
+					<div key={movie.id} className='movie-item col col-3'>
+						<img
+							className='card-img-top'
+							src={movie.movieImage}
+							alt='movie poster'
+						/>
+						<p className='movie-listing'>
+							{movie.movieName}
+							<br />
+							{movie.movieGenre}, {movie.releaseDate},
+							<br />
+							Rated {movie.movieRating}
+							<br />
+						</p>
+					</div>
+					{movie.reviews?.map((review) => (
+						<div key={review.id} class='reviews'>
+							{review.movieRating}
+							<br />"{review.review}"<br />—
+							{review.username}, {review.currentDate}
 						</div>
-					))
-					}
-			</ReactFragment>
-				))
-			<>
-				<ReviewForm />
-			</>
+					))}
+					)
+					<>
+						<ReviewForm />
+					</>
+				</React.Fragment>
+			))}
 		</div>
 	);
 };
